0e304318766f4d10b4db9d81c356499a029639cf
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-06-23 21:37:07 +0100

    feat: demonstrate message passing between popup, background, content script

--- src/background.js
+++ src/background.js
@@ -2,4 +2,27 @@ import { logger } from './util/logger.js'
 
 logger.info(`background.js is running`)
 
-export const noop = () => {}
+function getSenderId(sender) {
+  if (sender.tab) {
+    return sender.tab.id
+  }
+  return sender.id
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  logger.info(`background.js received message`, message, sender)
+
+  if (!message || !message.type) {
+    logger.error(`unahndled message type`, message)
+  }
+
+  if (message.type === 'PING') {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      logger.info(`tabs`, tabs)
+      chrome.tabs.sendMessage(tabs[0].id, { type: "PING_CONTENT" }, (response) => {
+        logger.info(`response from content to ping`, response)
+      })
+    })
+    return sendResponse({ type: 'PING', data: 'pong' })
+  }
+})
