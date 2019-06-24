174251e87a262ee0f7bc3aaacc3903f47eee5bd7
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-06-24 20:05:19 +0100

    chore: remove comments

--- src/YouTubeTimeLoop.js
+++ src/YouTubeTimeLoop.js
@@ -13,42 +13,11 @@ class YouTubeTimeLoop {
     this.video = video
   }
 
-  // cancelTimeLoops = () => {
-  //   Object.keys(this.intervals).forEach(interval => {
-  //     clearInterval(interval)
-  //     delete this.intervals[interval]
-  //   })
-  // }
-
   play = startTime => {
     const start = YouTubeTimeLoop.convertToSeconds(startTime)
     this.video.currentTime = start
     this.video.play()
   }
-
-  // startLoop = (startTime, endTime) => {
-  //   const start = YouTubeTimeLoop.convertToSeconds(startTime)
-  //   const end = YouTubeTimeLoop.convertToSeconds(endTime)
-  //   const intervalLength = (end - start) * 1000
-  //   console.log(`intervalLength`, intervalLength)
-
-  //   console.log(`this.video`, this.video)
-
-  //   this.video.currentTime = start
-  //   this.video.play()
-
-  //   // const interval = setInterval(() => {
-  //   //   console.log(`tick`)
-  //   //   this.video.currentTime = start
-  //   // }, intervalLength)
-  //   // console.log(`interval`, interval)
-  //   // this.intervals[interval] = true
-
-  //   // chrome.alarms.create("video", {
-  //   //   when: Date.now(),
-  //   //   periodInMinutes: 0.1
-  //   // })
-  // }
 }
 
 const youtubeTimeLoop = new YouTubeTimeLoop()
