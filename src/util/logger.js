34f2d2d1d3b3f574352ce0b7378e1a37f53ec59d
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-07-15 00:43:00 +0100

    feat: add log levels

--- src/util/logger.js
+++ src/util/logger.js
@@ -15,24 +15,31 @@ const logLevels = [
 class Logger {
   constructor(props = {}) {
     this.logLevel = props.logLevel || DEFAULT_LOG_LEVEL
+    
+    this.setLogLevel = this.setLogLevel.bind(this)
+    this._output = this._output.bind(this)
+    this.info = this.info.bind(this)
+    this.error = this.error.bind(this)
   }
 
-  setLogLevel = level => this.logLevel = level
+  setLogLevel(level) {
+    this.logLevel = level
+  }
 
-  _output = (logLevel, ...args) => {
+  _output(logLevel, ...args) {
     const shouldOutput = logLevels.indexOf(logLevel) >= logLevels.indexOf(this.logLevel)
     if (shouldOutput) {
       console.log(`${logLevel}: `, ...args)
     }
   }
 
-  info = (...args) => {
+  info(...args) {
     this._output(INFO, ...args)
   }
 
-  error = (...args) => {
+  error(...args) {
     this._output(ERROR, ...args)
   }
 }
 
-export const logger = new Logger({ logLevel: SILENT })
+export const logger = new Logger({ logLevel: ERROR })
