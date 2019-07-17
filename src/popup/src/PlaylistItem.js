1e5fc07a4c43100916b01ae9ced2e06b2a5a3af7
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-07-17 20:38:21 +0100

    feat: style playlist item options

--- src/popup/src/PlaylistItem.js
+++ src/popup/src/PlaylistItem.js
@@ -2,24 +2,39 @@ import React from 'react'
 import { PlaylistItemOptions } from './PlaylistItemOptions'
 import './styles.css'
 
-export function PlaylistItem({
-  video: { id, name },
-  playVideo,
-  stopVideo,
-  isActive,
-  removeVideo
-}) {
-  return (
-    <div
-      key={id}
-      data-testid='playlist-item'
-      className={`playlist-item ${isActive ? 'active-video' : 'inactive-video'}`}
-    >
-      {isActive ? <button onClick={stopVideo}>Stop</button> : <button onClick={playVideo}>Play</button>}
-      <div className='playlist-item-details'>
-        {name}
+export class PlaylistItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showOptions: false
+    }
+  }
+
+  toggleOptions = () => {
+    this.setState(prevState => ({
+      showOptions: !prevState.showOptions
+    }))
+  }
+
+  render() {
+    const { showOptions } = this.state
+    const { video: { id, name }, isActive, playVideo, stopVideo, removeVideo } = this.props
+    return (
+      <div
+        key={id}
+        data-testid='playlist-item'
+        className={`playlist-item ${isActive ? 'active-video' : 'inactive-video'}`}
+      >
+        {!showOptions && (isActive ? <button onClick={stopVideo}>Stop</button> : <button onClick={playVideo}>Play</button>)}
+        <div className='playlist-item-details'>
+          {name}
+        </div>
+        <PlaylistItemOptions
+          showOptions={showOptions}
+          toggleOptions={this.toggleOptions}
+          removeVideo={removeVideo}
+        />
       </div>
-      <PlaylistItemOptions removeVideo={removeVideo} />
-    </div>
-  )
+    )
+  }
 }
--- src/popup/src/PlaylistItemOptions.js
+++ src/popup/src/PlaylistItemOptions.js
@@ -1,45 +1,21 @@
 import React from 'react'
 import './styles.css'
 
-export class PlaylistItemOptions extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isExpanded: false
-    }
-  }
-
-  toggleOptions = () => {
-    this.setState(prevState => ({
-      isExpanded: !prevState.isExpanded
-    }))
-  }
-
-  renderOptions() {
-    return <div>
-      <button onClick={this.removeVideo}>
-        Remove
-      </button>
-    </div>
-  }
-
-  removeVideo = async () => {
-    await this.props.removeVideo()
-  }
-
-  render() {
-    const { isExpanded } = this.state
-    return (
-      <div className='playlist-item-options'>
-        {isExpanded && this.renderOptions()}
-        <div
-          data-testid='playlist-item-options-toggle'
-          className={`${isExpanded ? 'expanded' : 'colllapsed'}`}
-          onClick={this.toggleOptions}
-        >
-          Options
-        </div>
+export function PlaylistItemOptions ({ showOptions, toggleOptions, removeVideo }) {
+  return (
+    <div className='playlist-item-options'>
+      {showOptions && (
+        <div>
+          <button onClick={removeVideo}>
+            Remove
+          </button>
       </div>
-    )
-  }
+      )}
+      <div
+        data-testid='playlist-item-options-toggle'
+        className={`toggle ${showOptions ? 'expanded' : 'collapsed'}`}
+        onClick={toggleOptions}
+      />
+    </div>
+  )
 }
